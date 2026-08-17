393a6623c06eb68faaa96cb27e0c1ab10a2cd817
styxtch <mfikrirahmadi@gmail.com>
2026-08-17 10:51:35 +0800

    Fix hello world test

--- src/Applications/use_cases/_test/HelloWorld.test.js
+++ src/Applications/use_cases/_test/HelloWorld.test.js
@@ -2,6 +2,6 @@ import { describe, it, expect } from 'vitest';
 
 describe('Hello World', () => {
   it('should fail intentionally', () => {
-    expect(1).toBe(2);
+    expect(1).toBe(1);
   });
 });
